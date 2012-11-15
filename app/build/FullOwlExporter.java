117cab0f29373dd6715fd20f27934c05f2a220c4
loopasam <samuel.croset@gmail.com>
2012-11-15 16:30:42 +0000

    GO to OWL done
    Signed-off-by: loopasam <samuel.croset@gmail.com>

--- app/build/FullOwlExporter.java
+++ app/build/FullOwlExporter.java
@@ -2,10 +2,16 @@ package build;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import play.Logger;
 
 import uk.ac.ebi.brain.core.Brain;
+import uk.ac.ebi.brain.error.BadNameException;
 import uk.ac.ebi.brain.error.BadPrefixException;
 import uk.ac.ebi.brain.error.BrainException;
+import uk.ac.ebi.brain.error.ExistingClassException;
 import uk.ac.ebi.brain.error.NewOntologyException;
 import uk.ac.ebi.brain.error.StorageException;
 
@@ -16,11 +22,59 @@ public class FullOwlExporter extends OwlExporter {
 	}
 
 	@Override
-	public void start() throws FileNotFoundException, IOException, ClassNotFoundException {
+	public void start() throws FileNotFoundException, IOException, ClassNotFoundException, BrainException {
+		
+		//Converts GO into OWL
+		
+		Logger.info("Converting GO into OWL...");
 		GeneOntology go = new GeneOntology("data/tmp/go.ser");
+		Logger.info("Adding the Go terms to the Brain...");
+		//Adds just the terms first
+		for (GoTerm goTerm : go.getBioProcessesAndMolecularFunctions()) {
+			String classId = goTerm.getId().replaceAll(":", "_");
+			if(!this.getBrain().knowsClass(classId)){
+				this.getBrain().addClass("http://purl.obolibrary.org/obo/" + classId);
+				this.getBrain().label(classId, goTerm.getName());
+			}else{
+				Logger.warn("The class '"+classId+"' ("+goTerm.getName()+") already exists");
+			}
+		}
+
+		//Not hyper-optimised but kept separated for the sake of clarity.
+		Logger.info("Adding the GO axioms to the Brain...");
+
+		//Stored the relations not supported by the FTC at the time being
+		Set<String> unsupportedRelations = new HashSet<String>();
+
+		for (GoTerm goTerm : go.getBioProcessesAndMolecularFunctions()) {
+			String classId = goTerm.getId().replaceAll(":", "_");
+
+			for (GoRelation relation : goTerm.getRelations()) {
+				if(relationSupported(relation) || relation.getType().equals("is_a")){
+					String parentId = relation.getTarget().replaceAll(":", "_");
+					String expression = this.getGoRelationsMapper().get(relation.getType()) + " some " + parentId;
+
+					if(relation.getType().equals("is_a")){
+						this.getBrain().subClassOf(classId, parentId);
+					}else{
+						this.getBrain().subClassOf(classId, expression);
+					}
+
+				}else{
+					unsupportedRelations.add(relation.getType());
+				}
+			}
+		}
+
+		//List the unsupported relations - information
+		for (String unsupportedRelation : unsupportedRelations) {
+			Logger.info("The relation '"+unsupportedRelation+"' present in the GO is not supported in the FTC");
+		}
+		
+		//End conversion GO
+		
 		
 	}
-
 
 
 }
--- app/build/OwlExporter.java
+++ app/build/OwlExporter.java
@@ -2,6 +2,9 @@ package build;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+
+import play.Logger;
 
 import uk.ac.ebi.brain.core.Brain;
 import uk.ac.ebi.brain.error.BadNameException;
@@ -17,6 +20,7 @@ public abstract class OwlExporter {
 	private Brain brain;
 	public static String PREFIX = "http://www.ebi.ac.uk/ftc/";
 	private String ontologyName;
+	private HashMap<String, String> goRelationsMapper;
 
 	public String getOntologyName() {
 		return ontologyName;
@@ -34,6 +38,14 @@ public abstract class OwlExporter {
 		this.pathOut = pathOut;
 	}
 
+	public HashMap<String, String> getGoRelationsMapper() {
+		return goRelationsMapper;
+	}
+
+	public void setGoRelationsMapper(HashMap<String, String> goRelationsMapper) {
+		this.goRelationsMapper = goRelationsMapper;
+	}
+
 	public Brain getBrain() {
 		return brain;
 	}
@@ -42,17 +54,21 @@ public abstract class OwlExporter {
 		this.brain = brain;
 	}
 
+	//TODO comment on what this is doing 
 	public OwlExporter(String pathOut, String ontologyName) throws BrainException {
 		this.setPathOut(pathOut);
 		this.setOntologyName(ontologyName);
-				
+
 		Brain brain = new Brain(PREFIX, this.getOntologyName());
+		brain.prefix("http://purl.uniprot.org/core/", "uniprot");
+		brain.prefix("http://purl.obolibrary.org/obo/", "obo");
+
 
 		//TBox specifications - Core external classes
 
 		brain.addClass("http://purl.obolibrary.org/obo/GO_0003674");
 		brain.label("GO_0003674", "molecular function");
-		
+
 		brain.addClass("http://purl.obolibrary.org/obo/GO_0008150");
 		brain.label("GO_0008150", "biological process");
 		
@@ -61,16 +77,16 @@ public abstract class OwlExporter {
 		
 		brain.addClass("http://purl.obolibrary.org/obo/CHEBI_24431");
 		brain.label("CHEBI_24431", "chemical entity");
-		
+
 		brain.addClass("http://purl.obolibrary.org/obo/CHEBI_50906");
 		brain.label("CHEBI_50906", "role");
-		
+
 		//FTC specification
-		
+
 		brain.addClass("FTC_C1");
 		brain.label("FTC_C1", "agent");
 		brain.subClassOf("FTC_C1", "CHEBI_50906");
-		
+
 		//RBox specifications
 
 		//GO RBox logic - See http://www.geneontology.org/GO.ontology-ext.relations.shtml
@@ -103,12 +119,33 @@ public abstract class OwlExporter {
 		brain.label("FTC_R2", "has-function");
 
 		this.setBrain(brain);
+
+		//Mapper to convert strings from the GO.ser into OWL properties
+
+		HashMap<String, String> goRelationMapper = new HashMap<String, String>();
+		goRelationMapper.put("part_of", "BFO_0000050");
+		goRelationMapper.put("regulates", "RO_0002211");
+		goRelationMapper.put("positively_regulates", "RO_0002213");
+		goRelationMapper.put("negatively_regulates", "RO_0002212");
+		goRelationMapper.put("has_part", "BFO_0000051");
+		this.setGoRelationsMapper(goRelationMapper);
 	}
 
-	public abstract void start() throws FileNotFoundException, IOException, ClassNotFoundException;
+	public abstract void start() throws FileNotFoundException, IOException, ClassNotFoundException, BrainException;
 
 	public void save() throws StorageException {
+		Logger.info("Saving the OWL file...");
 		this.getBrain().save(this.getPathOut());
 	}
+
+	protected boolean relationSupported(GoRelation relation) {
+
+		if(this.getGoRelationsMapper().containsKey(relation.getType())){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
 
 }
