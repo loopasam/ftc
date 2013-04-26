742c7699c968ce0268537187ff56838822ba952c
loopasam <samuel.croset@gmail.com>
2013-04-26 16:09:19 +0100

    R3C figure series
    Signed-off-by: loopasam <samuel.croset@gmail.com>

--- data/analysis/figures/R3C.R
+++ data/analysis/figures/R3C.R
@@ -2,7 +2,7 @@
 # Only compounds that are present in different categories (based on a treshold) are
 # represented.
 
-values <- read.csv("/home/samuel/git/ftc/data/analysis/diff_cats_1lvl.csv", 
+values <- read.csv("/home/samuel/git/ftc/data/analysis/same_cats_1lvl.csv", 
                    head=TRUE, 
                    sep=","
 )
@@ -12,10 +12,14 @@ struc <- values$secondSim
 
 plot(struc, 
      moa,
-     pch='*',
-     main='Structural similarity versus mode of action similarity\nOnly compunds in different ATC categories (one level)', 
+     xlim=c(0,1),
+     ylim=c(0,1),
+     pch='.',
+     main='Structural similarity versus mode of action similarity\nOnly compounds present in the same ATC categories (four level)', 
      ylab="Mode of action similarity (Jaccard index over ancestor classes)",
      xlab="Structural similarity (Tanimoto coeff. over hybridization fingerprint)",
+     col='red'
 )
-
 abline(h=0.5, v=0.5)
+
+par(new=TRUE)
