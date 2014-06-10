6fc69cedb517c8de1124dcbe93739727986579cb
loopasam <samuel.croset@gmail.com>
2014-06-10 15:28:56 +0100

    p-value analysis

--- data/analysis/pvalues/signifiance_disease_cat.R
+++ data/analysis/pvalues/signifiance_disease_cat.R
@@ -1,9 +1,9 @@
-same <- read.csv("/home/samuel/git/ftc/data/analysis/pvalues/pval_V_same.csv", 
+same <- read.csv("/home/loopasam/git/ftc/data/analysis/pvalues/pval_A_same.csv", 
                  header=FALSE, 
                  sep=","
 )
 
-diff <- read.csv("/home/samuel/git/ftc/data/analysis/pvalues/pval_V_diff.csv", 
+diff <- read.csv("/home/loopasam/git/ftc/data/analysis/pvalues/pval_A_diff.csv", 
                  header=FALSE, 
                  sep=","
 )
@@ -14,4 +14,12 @@ diff <- as.matrix(diff);
 mean(diff)
 mean(same) - mean(diff)
 library(DAAG)
-twot.permutation(x1=diff, x2=same, nsim=10000, plotit=TRUE)
+twot.permutation(x1=diff, x2=same, nsim=1000000, plotit=TRUE)
+
+densitydiff <- density(diff)
+plot( densitydiff, col=rgb(0,0,1,1/4), xlim=c(0,1), lwd=8)
+abline(v=mean(diff), col=rgb(0,0,1,1/4), lwd=2)
+densitysame <- density(same)
+par(new=T)
+plot( densitysame, col=rgb(1,0,0,1/4), xlim=c(0,1), lwd=8)  # second
+abline(v=mean(diff), col=rgb(1,0,0,1/4), lwd=2)
