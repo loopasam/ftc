97e4b744b283ef98e03e3f2911b13db955c628dd
loopasam <samuel.croset@gmail.com>
2014-06-10 16:51:59 +0100

    batch calculation p-values

--- data/analysis/pvalues/signifiance_disease_cat.R
+++ data/analysis/pvalues/signifiance_disease_cat.R
@@ -1,20 +1,27 @@
-same <- read.csv("/home/loopasam/git/ftc/data/analysis/pvalues/pval_A_same.csv", 
-                 header=FALSE, 
-                 sep=","
-)
-
-diff <- read.csv("/home/loopasam/git/ftc/data/analysis/pvalues/pval_A_diff.csv", 
-                 header=FALSE, 
-                 sep=","
-)
-
-same <- as.matrix(same);
-mean(same)
-diff <- as.matrix(diff);
-mean(diff)
-mean(same) - mean(diff)
+codes <- c("A","B","C", "D", "G", "H", "J", "L", "M", "N", "P", "R", "S", "V");
 library(DAAG)
-twot.permutation(x1=diff, x2=same, nsim=1000000, plotit=TRUE)
+path = "/home/loopasam/git/ftc/data/analysis/pvalues/";
+
+for(code in codes) {
+  print(code)
+  file <- paste(path,"pval_", code,"_same.csv",sep="")
+  same <- read.csv(file, 
+                 header=FALSE, 
+                 sep=","
+  )
+  file <- paste(path,"pval_", code,"_diff.csv",sep="")
+  diff <- read.csv(file, 
+                 header=FALSE, 
+                 sep=","
+  )
+
+  same <- as.matrix(same);
+  # mean(same)
+  diff <- as.matrix(diff);
+  # mean(diff)
+  # mean(same) - mean(diff)
+  twot.permutation(x1=diff, x2=same, nsim=1000000, plotit=TRUE)
+}
 
 densitydiff <- density(diff)
 plot( densitydiff, col=rgb(0,0,1,1/4), xlim=c(0,1), lwd=8)
